cb6f15a3eda543ad1504e3961a64ed3b24a5f0ac
Vincent Delitz <vincent.delitz@tum.de>
2023-07-14 18:50:41 +0200

    make first version working

---
+++ backend-nodejs/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const Corbado = require('@corbado/node-sdk');
+require('dotenv').config()
+
+
+
+
+const projectID = process.env.PROJECT_ID;
+const apiSecret = process.env.API_SECRET;
+console.log(projectID)
+console.log(apiSecret)
+const config = new Corbado.Configuration(projectID, apiSecret);
+const corbado = new Corbado.SDK(config);
+
+app.use(cors()); // handle CORS issue
+app.use(express.json());
+
+app.post('/api/createAssociationToken', async (req, res) => {
+
+    console.log(req.body)
+    const {loginIdentifier, loginIdentifierType} = req.body;
+    const clientInfo = corbado.utils.getClientInfo(req);
+
+    try {
+        console.log(corbado)
+        let associationToken = await corbado.associationTokens.create(loginIdentifier, loginIdentifierType, clientInfo);
+        console.log(associationToken.data.token);
+
+        res.send(associationToken?.data?.token).status(200);
+    } catch (err) {
+        console.log(err)
+        res.send("Error occured.").status(500);
+    }
+});
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+});
