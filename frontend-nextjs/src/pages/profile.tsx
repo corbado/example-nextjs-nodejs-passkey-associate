c65a205a8dc65db1e4398b826d4ea8b46924dafc
Incorbador <incorbador@gmx.de>
2023-07-16 22:12:35 +0200

    small changes, mostly FE

--- frontend-nextjs/src/pages/profile.tsx
+++ frontend-nextjs/src/pages/profile.tsx
@@ -1,73 +1,51 @@
-import {useRef, useState, useEffect, useCallback} from "react";
+import {useState, useEffect, useCallback} from "react";
 import axios from "axios";
 
 export default function Profile() {
-    const [associateToken, setAssociateToken] = useState(null);
-    const refPasskeyAssociate = useRef();
+    const [associateToken, setAssociateToken] = useState();
+    const [ref, setRef] = useState()
 
-    // @ts-ignore
-    const handleEvent = useCallback((event) => {
-        switch (event.detail[0].type) {
-            case "PASSKEY_CREATION_SUCCESSFUL": {
-                console.log("PASSKEY_CREATION_SUCCESSFUL");
-                break;
-            }
-            case "PASSKEY_CREATION_FAILED": {
-                console.log("PASSKEY_CREATION_FAILED");
-                break;
-            }
-            case "DEVICE_NOT_PASSKEY_READY": {
-                console.log("DEVICE_NOT_PASSKEY_READY");
-                break;
-            }
-            default:
-                console.log("default")
-        }
-    }, []);
+    const onDeviceNotPasskeyReady = useCallback((_event) => {
+        console.log("device is not ready for passkeys")
+    }, [])
+
+    const onPasskeyCreationFailed = useCallback((event) => {
+        console.log("passkey could not be created due to this error")
+        console.error(event.detail)
+    }, [])
+
+    const onPasskeyCreationSuccessful = useCallback((_event) => {
+        console.log("passkey was created successfully")
+    }, [])
 
     useEffect(() => {
-        const element = refPasskeyAssociate.current;
-        const events = ['device-not-passkey-ready', 'passkey-creation-successful', 'passkey-creation-failed'];
-
-
-        if (element) {
-            events.forEach(event => {
-                // @ts-ignore
-                element.addEventListener(event, handleEvent);
-            })
+        if (ref) {
+            ref.addEventListener('device-not-passkey-ready', onDeviceNotPasskeyReady)
+            ref.addEventListener('passkey-creation-successful', onPasskeyCreationSuccessful)
+            ref.addEventListener('passkey-creation-failed', onPasskeyCreationFailed)
         }
-
 
         // Cleanup function
         return () => {
-            if (element) {
-                events.forEach(event => {
-                    // @ts-ignore
-                    element.removeEventListener(event, handleEvent);
-                })
+            if (ref) {
+                ref.removeEventListener('device-not-passkey-ready', onDeviceNotPasskeyReady)
+                ref.removeEventListener('passkey-creation-successful', onPasskeyCreationSuccessful)
+                ref.removeEventListener('passkey-creation-failed', onPasskeyCreationFailed)
             }
-
         };
-    }, [handleEvent])
+
+    }, [ref, onDeviceNotPasskeyReady, onPasskeyCreationSuccessful, onPasskeyCreationFailed])
 
 
     const handleButtonClick = async () => {
         try {
-            axios.post(process.env.NEXT_PUBLIC_API_BASE_URL + "/api/createAssociationToken", {
+            const response = await axios.post("http://localhost:3001/api/createAssociationToken", {
                 loginIdentifier: "test5@web.de",
                 loginIdentifierType: "email"
-            }, {
-                headers: {
-                    "Content-Type": "application/json",
-                },
             })
 
-                .then(response => {
-
-                    console.log("associateToken: ", associateToken)
-                    // @ts-ignore
-                    setAssociateToken(response.data);
-                })
+            // @ts-ignore
+            setAssociateToken(response.data);
         } catch (err) {
             console.log(err)
         }
@@ -81,10 +59,10 @@ export default function Profile() {
             <button onClick={handleButtonClick}>Add passkey to my account</button>
             {associateToken &&
                 <corbado-passkey-associate
-                    ref={refPasskeyAssociate}
+                    ref={setRef}
                     project-id={process.env.NEXT_PUBLIC_PROJECT_ID}
                     association-token={associateToken}
-                ></corbado-passkey-associate>}
+                />}
         </div>
     )
 }
